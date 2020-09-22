5bc40f86eb8d868ba4d36b6a8ea3dd4fc8e5c6cb
juanbaltasar <martinjuanbaltasar@gmail.com>
2020-09-22 10:46:00 -0300

    TP Terminado

--- app.js
+++ app.js
@@ -16,29 +16,85 @@ new Vue({
             return `${salud}%`
         },
         empezarPartida: function () {
+            this.hayUnaPartidaEnJuego = true;
+            this.saludJugador = 100;
+            this.saludMonstruo = 100;
         },
         atacar: function () {
+            let daño = this.calcularHeridas(this.rangoAtaque);
+            this.saludMonstruo -= daño;
+
+            this.registrarEvento({esJugador:true,text:'El jugador hiere al monstruo por ' + daño + ' de vida con un ataque normal'});
+
+            if (this.verificarGanador()) {
+                return;
+            }
+
+            this.ataqueDelMonstruo();
         },
 
         ataqueEspecial: function () {
+            let daño = this.calcularHeridas(this.rangoAtaqueEspecial);
+            this.saludMonstruo -= daño;
+
+            this.registrarEvento({esJugador:true,text:'El jugador hiere al monstruo por ' + daño + ' de vida con un ataque especial'});
+
+            if (this.verificarGanador()) {
+                return;
+            }
+
+            this.ataqueDelMonstruo();
         },
 
         curar: function () {
+            (this.saludJugador > 90 ? this.saludJugador = 100 : this.saludJugador += 10);
+
+            this.registrarEvento({esJugador:true,text:(this.saludJugador === 100 ? 'El jugador se cura y rellena su vida por completo' : 'El jugador se cura por 10 de vida')});
+
+            this.ataqueDelMonstruo();
         },
 
         registrarEvento(evento) {
+            this.turnos.unshift(
+                {esJugador : evento.esJugador, text : evento.text}
+                );
         },
         terminarPartida: function () {
+            this.hayUnaPartidaEnJuego = false;
+            this.turnos = [];
         },
 
         ataqueDelMonstruo: function () {
+            let daño = this.calcularHeridas(this.rangoAtaqueDelMonstruo);
+            this.saludJugador -= daño;
+
+            this.registrarEvento({esJugador:false,text:'El monstruo hiere al jugador por ' + daño + ' de vida'});
+
+            if (this.verificarGanador()) {
+                return;
+            }
         },
 
         calcularHeridas: function (rango) {
-            return 0
-
+            return Math.max(Math.floor(Math.random() * rango[1] + 1), rango[0]);
         },
         verificarGanador: function () {
+            if(this.saludMonstruo <= 0)
+            {
+                this.terminarPartida();
+                if (confirm("Ganaste, jugar de nuevo?")) {
+                    this.empezarPartida();
+                }
+                return true;
+            }
+            if(this.saludJugador <= 0)
+            {
+                this.terminarPartida();
+                if (confirm("Perdiste, jugar de nuevo?")) {
+                    this.empezarPartida();
+                }
+                return true;
+            }
             return false;
         },
         cssEvento(turno) {
@@ -46,7 +102,7 @@ new Vue({
             return {
                 'player-turno': turno.esJugador,
                 'monster-turno': !turno.esJugador
-            }
+            };
         }
     }
 });
